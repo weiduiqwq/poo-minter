ca820dc238d1f58349d94c400d6ed25566aae307
2xqian <102758126+weiduiqwq@users.noreply.github.com>
2024-05-11 12:49:27 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,70 @@
 package main
 
 import (
+	"bufio"
 	"context"
-	"github.com/gogf/gf/v2/frame/g"
+	"fmt"
 	"github.com/gogf/gf/v2/os/genv"
-	"github.com/gogf/gf/v2/os/glog"
+	"log"
+	"os"
 	"poo/poo_minter"
+	"sync"
 )
-
-func init() {
-	g.Log().SetLevel(glog.LEVEL_ALL ^ glog.LEVEL_DEBU)
-}
 
 func main() {
 	ctx := context.Background()
-	minter := poo_minter.NewPooMinter(genv.GetWithCmd("INIT_DATA").String())
-	err := minter.Mint(ctx)
-	if err != nil {
-		g.Log().Fatalf(ctx, "%+v", err)
+
+	// 从INIT_DATA指定的路径获取数据
+	filePath := genv.GetWithCmd("INIT_DATA").String()
+
+	// 检查文件是否存在
+	fileInfo, err := os.Stat(filePath)
+	if err == nil && !fileInfo.IsDir() {
+		// 文件存在且不是目录，按照文件方式处理
+
+		file, err := os.Open(filePath)
+		if err != nil {
+			log.Fatalf("无法打开文件：%v", err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+
+		// 使用 WaitGroup 等待所有线程完成
+		var wg sync.WaitGroup
+
+		for scanner.Scan() {
+			minterData := scanner.Text()
+			fmt.Println(minterData)
+
+			// 为每个minter启动一个新线程
+
+			wg.Add(1)
+			go func(data string) {
+				defer wg.Done()
+
+				// 创建独立的minter实例
+				minter := poo_minter.NewPooMinter(data)
+
+				// 运行minter.Mint(ctx)
+				err := minter.Mint(ctx)
+				if err != nil {
+					log.Printf("minter.Mint 错误：%+v", err)
+				}
+			}(minterData)
+		}
+
+		wg.Wait()
+
+		if err := scanner.Err(); err != nil {
+			log.Fatalf("读取文件错误：%v", err)
+		}
+
+	} else {
+		minter := poo_minter.NewPooMinter(filePath)
+		err := minter.Mint(ctx)
+		if err != nil {
+			log.Fatalf("%+v", err)
+		}
 	}
 }
